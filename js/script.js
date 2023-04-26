5cc18d936ab8b32c26082e709147ea895bba4b2f
aichurokdev <aichurokkuttubekovna1@gmail.com>
2023-04-26 15:42:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    slidesPerView: 4,
+    // spaceBetween:0,
+    // freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
